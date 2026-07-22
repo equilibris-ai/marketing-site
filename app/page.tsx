233da7508d26479e3903e57db67c2056ad7f94c5
Konstantin Gredeskoul <kigster@gmail.com>
2026-07-21 21:42:43 -0700

    Fixing the label

--- app/page.tsx
+++ app/page.tsx
@@ -48,8 +48,7 @@ export default function Home() {
                 Join the waitlist
               </button>
               <p className="fine">
-                A few quick questions to see if we&rsquo;re a fit. No spam &mdash;
-                early access invites go out first-come.
+              No spam. Early access invites go out first-come. Unsubscribe anytime.
               </p>
             </div>
           </section>
